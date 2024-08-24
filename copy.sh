3a5574d66a8689c92217b5a3d9b9f99c82cd9eff
Matt Dlubac <matt@dlubac.com>
2024-08-23 21:38:17 -0400

    V497 of this site

--- copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SRC_DIR="$(pwd)/portfolio/"
-DEST_DIR="$(pwd)"
-
-# use cp instead of rsync
-cp -r "$SRC_DIR" "$DEST_DIR"
---
+++ photo-reel/photo-reel.js
@@ -0,0 +1,33 @@
+const images = [
+    {
+        "path": "_7R53080-Enhanced-NR.avif",
+        "thumbnail": "_7R53080-Enhanced-NR_thumb.avif",
+        "height": "233px",
+        "width": "350px"
+    },
+    {
+      "path": "_7R53403-Enhanced-NR.avif",
+      "thumbnail": "_7R53403-Enhanced-NR_thumb.avif",
+      "width": "350px",
+      "height":  "280px"
+    },
+    {
+        "path": "_R000366-Enhanced-NR.avif",
+        "thumbnail": "_R000366-Enhanced-NR_thumb.avif",
+        "width": "350px",
+        "height": "525px",
+
+    },
+    {
+        "path": "_R000368-Enhanced-NR.avif",
+        "thumbnail": "_R000368-Enhanced-NR_thumb.avif",
+        "height": "525px",
+        "width": "350px"
+    },
+    {
+        "path": "_R000398-Enhanced-NR.avif",
+        "thumbnail": "_R000398-Enhanced-NR_thumb.avif",
+        "height": "437px",
+        "width": "350px"
+    },
+]
